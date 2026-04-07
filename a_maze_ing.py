b98593889d3bfa2a17754bbce8f3297f616f82c7
Pacome Pilaz <papilaz@z4r5p2.42lyon.fr>
2026-04-07 18:15:53 +0200

    rename main

---
+++ a_maze_ing.py
@@ -0,0 +1,16 @@
+from maze.parsing import clear, parsing_data
+from maze.labyrinth import labyrinth_generation
+
+
+def main() -> None:
+    file_config = "config.txt"
+    data: dict = {}
+    if parsing_data(file_config) is not False:
+        data = parsing_data(file_config)
+        clear()
+        labyrinth_generation(data)
+        print(data)
+
+
+if __name__ == "__main__":
+    main()
