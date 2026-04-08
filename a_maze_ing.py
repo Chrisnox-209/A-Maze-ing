eb20f36b196a5a4d532e73d61ed88d33f7e7a16e
Pacome Pilaz <papilaz@z4r4p6.42lyon.fr>
2026-04-09 01:30:34 +0200

    ➕ add():

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,5 +1,6 @@
 from utils.parser import clear, parsing_data
 from maze.maze import Maze
+from menu import Menu
 
 
 def main() -> None:
@@ -7,14 +8,16 @@ def main() -> None:
     data: dict = {}
     if parsing_data(file_config) is not False:
         data = parsing_data(file_config)
-        clear()
         maze = Maze(data)
-        maze.draw_maze()
-        print(data)
-        print(f"L'ID de la cellule (x=0, y=0) est : {maze.grid[0][0].cell_id}")
-        print(f"L'ID de la cellule (x=0, y=10) est : {maze.grid[0][9].cell_id}")
-        print(f"L'ID de la cellule (x=2, y=1) est : {maze.grid[1][2].cell_id}")
+        maze.create_logo()
+        Menu(maze)
+      #   maze.draw_maze(None)
+      #   print(data)
+      #   print(f"L'ID de la cellule (x=0, y=0) est : {maze.grid[0][0].cell_id}")
+      #   print(f"L'ID de la cellule (x=0, y=10) est : {maze.grid[0][9].cell_id}")
+      #   print(f"L'ID de la cellule (x=2, y=1) est : {maze.grid[1][2].cell_id}")
 
 
 if __name__ == "__main__":
+    clear()
     main()
