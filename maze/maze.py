80eab05a4d301bb6a92baee50728478f7d99af93
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-04-08 15:02:13 +0200

    🔨 refactor(maze): organisation du code

--- maze/maze.py
+++ maze/maze.py
@@ -1,33 +1,6 @@
 import sys
 from typing import Any, Literal
-from enum import Enum
-
-
-class Draw(Enum):
-    TOP_WALL = "█▀▀"
-    TOP_PATH = "█  "
-    MID_WALL = "█  "
-    MID_PATH = "   "
-    V_LINE = "█"
-    CORNER = "█"
-    BOTTOM = "▀▀"
-    CORNER_BOT = "▀"
-
-
-class Color(Enum):
-    RESET = '\033[0m'
-    DEFAULT = '\033[0m'
-
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-
-
+from maze.utils_enum import Color, Draw
 
 class Cell:
     def __init__(self, x: int, y: int) -> None:
@@ -39,10 +12,11 @@ class Cell:
             "South": True,
             "West": True
         }
+        self.visitee = False
         self.id: int = 0
 
     def __repr__(self) -> str:
-        return f"{self.x}:{self.y}"
+        return f"{self.x}:{self.y}, id={self.id}"
 
 
 class Maze:
