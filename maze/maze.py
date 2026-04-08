45006e739d002f4a5173bf06e60307866b954620
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-04-08 16:58:11 +0200

    🔨 refactor(maze): modification du style de la grille et ajout de plusieurs style

--- maze/maze.py
+++ maze/maze.py
@@ -1,56 +1,88 @@
-import sys
-from typing import Any, Literal
-from maze.utils_enum import Color, Draw
+from maze.utils_enum import Wall, WallDouble, WallSkinny, WallRetro, WallUgly
+from typing import Any
+import time
+import random
+
 
 class Cell:
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x: int, y: int, cell_id: int) -> None:
         self.x: int = int(x)
         self.y: int = int(y)
+        self.cell_id: int = int(cell_id)
+        self.visitee: bool = False
         self.walls: dict = {
             "North": True,
-            "East": False,
+            "East": True,
             "South": True,
             "West": True
         }
-        self.visitee = False
-        self.id: int = 0
 
     def __repr__(self) -> str:
-        return f"{self.x}:{self.y}, id={self.id}"
+        return f"{self.x}:{self.y}, id={self.cell_id}"
 
 
 class Maze:
     def __init__(self, data: dict) -> None:
         self.width: int = data["WIDTH"]
         self.height: int = data["HEIGHT"]
-        self.grid: list[list[Cell]] = [[Cell(x_idx, y_idx)
-                                        for x_idx in range(self.width)]
-                                       for y_idx in range(self.height)]
+        self.grid: list[list[Cell]] = []
+
+        for y in range(self.height):
+            row: list[Cell] = []
+            for x in range(self.width):
+                cell_id: int = y * self.width + x
+                row.append(Cell(x, y, cell_id))
+            self.grid.append(row)
 
     def set_color() -> None:
         pass
 
     def draw_maze(self) -> None:
-        self.grid[5][4].walls["North"] = False
-        self.grid[0][0].walls["North"] = False
-        for y in range(self.height):
-            for x in range(self.width):
-                data_cell = self.grid[y][x]
-                if data_cell.walls["North"]:
-                    print(Draw.TOP_WALL.value, end="")
-                else:
-                    print(Draw.TOP_PATH.value, end="")
-            print(Draw.CORNER.value)
+        delais = 0.001
+        list_style: list[str] = [Wall,
+                                 WallDouble,
+                                 WallSkinny,
+                                 WallRetro,
+                                 WallUgly]
+        wall: str = random.choice(list_style)
 
-            for _ in range(1):
-                for x in range(self.width):
-                    data_cell: Cell = self.grid[y][x]
-                    if data_cell.walls["West"]:
-                        print(Draw.MID_WALL.value, end="")
-                    else:
-                        print(Draw.MID_PATH.value, end="")
-                print(Draw.V_LINE.value)
+        for y in range(self.height):
+
+            for x in range(self.width):
+                if y == 0:
+                    inter: Any = wall.TOP_LEFT.value if x == 0 else wall.T_TOP.value
+                else:
+                    inter = wall.T_LEFT.value if x == 0 else wall.CROSS.value
+                time.sleep(delais)
+                print(inter, end="")
+
+                if self.grid[y][x].walls["North"]:
+                    time.sleep(delais)
+                    print(wall.H_LINE.value, end="", flush=True)
+                else:
+                    time.sleep(delais)
+                    print(wall.H_PATH.value, end="", flush=True)
+
+            if y == 0:
+                time.sleep(delais)
+                print(wall.TOP_RIGHT.value)
+            else:
+                time.sleep(delais)
+                print(wall.T_RIGHT.value)
+
+            for x in range(self.width):
+                if self.grid[y][x].walls["West"]:
+                    time.sleep(delais)
+                    print(wall.V_LINE.value, end="", flush=True)
+                else:
+                    time.sleep(delais)
+                    print(wall.V_PATH.value, end="", flush=True)
+            time.sleep(delais)
+            print(wall.V_RIGHT.value)
 
         for x in range(self.width):
-            print(Draw.CORNER_BOT.value + Draw.BOTTOM.value, end="")
-        print(Draw.CORNER_BOT.value)
+            inter = wall.BOT_LEFT.value if x == 0 else wall.T_BOT.value
+            time.sleep(delais)
+            print(inter + wall.H_LINE.value, end="", flush=True)
+        time.sleep(delais)
+        print(wall.BOT_RIGHT.value)
