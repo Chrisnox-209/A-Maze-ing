42804be3f6eca59f96245e186486433f5b6f2841
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-04-08 16:41:25 +0200

    🧩 feat(Enum): Ajout de nouveaux Enum

--- maze/utils_enum.py
+++ maze/utils_enum.py
@@ -1,15 +1,89 @@
 from enum import Enum
 
-class Draw(Enum):
-    TOP_WALL = "█▀▀"
-    TOP_PATH = "█  "
-    MID_WALL = "█  "
-    MID_PATH = "   "
-    V_LINE = "█"
-    CORNER = "█"
-    BOTTOM = "▀▀"
-    CORNER_BOT = "▀"
 
+class WallDouble(Enum):
+    H_LINE = "═══"
+    H_PATH = "   "
+    V_LINE = "║   "
+    V_PATH = "    "
+    V_RIGHT = "║"
+    CROSS = "╬"
+    TOP_LEFT = "╔"
+    TOP_RIGHT = "╗"
+    BOT_LEFT = "╚"
+    BOT_RIGHT = "╝"
+    T_TOP = "╦"
+    T_BOT = "╩"
+    T_LEFT = "╠"
+    T_RIGHT = "╣"
+
+
+class Wall(Enum):
+    H_LINE = "━━━"
+    H_PATH = "   "
+    V_LINE = "┃   "
+    V_PATH = "    "
+    V_RIGHT = "┃"
+    CROSS = "╋"
+    TOP_LEFT = "┏"
+    TOP_RIGHT = "┓"
+    BOT_LEFT = "┗"
+    BOT_RIGHT = "┛"
+    T_TOP = "┳"
+    T_BOT = "┻"
+    T_LEFT = "┣"    
+    T_RIGHT = "┫"
+
+
+class WallSkinny(Enum):
+    H_LINE = "───"
+    H_PATH = "   "
+    V_LINE = "│   "
+    V_PATH = "    "
+    V_RIGHT = "│"
+    CROSS = "┼"
+    TOP_LEFT = "╭"
+    TOP_RIGHT = "╮"
+    BOT_LEFT = "╰"
+    BOT_RIGHT = "╯"
+    T_TOP = "┬"
+    T_BOT = "┴"
+    T_LEFT = "├"
+    T_RIGHT = "┤"
+
+
+class WallRetro(Enum):
+    H_LINE = "---"
+    H_PATH = "   "
+    V_LINE = "|   "
+    V_PATH = "    "
+    V_RIGHT = "|"
+    CROSS = "+"
+    TOP_LEFT = "+"
+    TOP_RIGHT = "+"
+    BOT_LEFT = "+"
+    BOT_RIGHT = "+"
+    T_TOP = "+"
+    T_BOT = "+"
+    T_LEFT = "+"
+    T_RIGHT = "+"
+
+
+class WallUgly(Enum):
+    H_LINE = "···"
+    H_PATH = "    "
+    V_LINE = "∶   "
+    V_PATH = "    "
+    V_RIGHT = "∶"
+    CROSS = "⊹"
+    TOP_LEFT = "┌"
+    TOP_RIGHT = "┐"
+    BOT_LEFT = "└"
+    BOT_RIGHT = "┘"
+    T_TOP = "┮"
+    T_BOT = "┶"
+    T_LEFT = "┟"
+    T_RIGHT = "┧"
 
 class Color(Enum):
     RESET = '\033[0m'
