22a3dbd3a1b37370df1530b3c00c63dc49950a7c
Pacome Pilaz <papilaz@z4r4p6.42lyon.fr>
2026-04-09 01:29:13 +0200

    🧩 feat(maze/): add logo 42 (voir vocal sur snap pour explication du probleme)

--- maze/maze-old.py
+++ maze/maze-old.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 
 class Draw(Enum):
-    TOP = "▀▀"  
+    TOP = "▀▀"
     BOTTOM = "▄▄"
     LATERAL = "█"
     EMPTY = "  "
@@ -18,26 +18,26 @@ class Draw(Enum):
 
 
 class Color(Enum):
-    RESET= '\033[0m'
+    RESET = '\033[0m'
     DEFAULT = '\033[0m'
 
-    BLACK   = '\033[30m'
-    RED     = '\033[31m'
-    GREEN   = '\033[32m'
-    YELLOW  = '\033[33m'
-    BLUE    = '\033[34m'
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
     MAGENTA = '\033[35m'
-    CYAN    = '\033[36m'
-    WHITE   = '\033[37m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
 
-    BG_BLACK   = '\033[40m'
-    BG_RED     = '\033[41m'
-    BG_GREEN   = '\033[42m'
-    BG_YELLOW  = '\033[43m'
-    BG_BLUE    = '\033[44m'
+    BG_BLACK = '\033[40m'
+    BG_RED = '\033[41m'
+    BG_GREEN = '\033[42m'
+    BG_YELLOW = '\033[43m'
+    BG_BLUE = '\033[44m'
     BG_MAGENTA = '\033[45m'
-    BG_CYAN    = '\033[46m'
-    BG_WHITE   = '\033[47m'
+    BG_CYAN = '\033[46m'
+    BG_WHITE = '\033[47m'
 
 
 def grid_logo_42(data: dict, grid: list[list[int]]) -> bool:
