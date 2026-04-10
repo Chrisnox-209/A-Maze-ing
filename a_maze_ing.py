e547fd6101c8edad1e6965fe795866c80c519ad9
Chris <christophe.pietrzak@gmail.com>
2026-04-10 11:21:09 +0200

    🛠 tweak(config): Changement de la config

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,19 +1,24 @@
 from utils.parser import clear, parsing_data
 from maze.maze import Maze
-from menu import Menu
+from maze.utils_enum import Wall
 
 
 def main() -> None:
     file_config = "config.txt"
     data: dict = {}
-    if parsing_data(file_config) is not False:
-        data = parsing_data(file_config)
-        maze = Maze(data)
-        maze.create_logo()
-        Menu(maze)
+    data = parsing_data(file_config)
+
+    if data is not False:
+        maze = Maze(data, Wall)
+        maze.logo()
+        clear()
+        maze.draw_maze()
+
+        # maze.create_logo()
+        # Menu(maze)
       #   maze.draw_maze(None)
       #   print(data)
-      #   print(f"L'ID de la cellule (x=0, y=0) est : {maze.grid[0][0].cell_id}")
+      #   print(f"L'ID de la cellule (x=0, y=0) est : {print")
       #   print(f"L'ID de la cellule (x=0, y=10) est : {maze.grid[0][9].cell_id}")
       #   print(f"L'ID de la cellule (x=2, y=1) est : {maze.grid[1][2].cell_id}")
 
