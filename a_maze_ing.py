b12238c027639cbe24a2eacf39ac95f32cda5da0
Chris <christophe.pietrzak@gmail.com>
2026-04-12 09:31:55 +0200

    🧩 feat(PARSING): Ajout de la class MaseConfig avec pydantic pour le parsing

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,26 +1,27 @@
-from utils.parser import clear, parsing_data
+from utils.parser import clear, parsing_data, MazeConfig
 from maze.maze import Maze
 from maze.utils_enum import Wall, WallUgly, WallDouble, WallRetro
 
 
 def main() -> None:
     file_config = "config.txt"
-    data: dict = {}
-    data = parsing_data(file_config)
+    config = parsing_data(file_config)
+    if config is False:
+        print(f"[ERROR]: file {file_config}")
+        return
+    maze = Maze(config, Wall)
+    maze.logo()
+    clear()
+    maze.draw_maze()
+    print(config)
 
-    if data is not False:
-        maze = Maze(data, Wall)
-        maze.logo()
-        clear()
-        maze.draw_maze()
-
-        # maze.create_logo()
-        # Menu(maze)
-      #   maze.draw_maze(None)
-      #   print(data)
-      #   print(f"L'ID de la cellule (x=0, y=0) est : {print")
-      #   print(f"L'ID de la cellule (x=0, y=10) est : {maze.grid[0][9].cell_id}")
-      #   print(f"L'ID de la cellule (x=2, y=1) est : {maze.grid[1][2].cell_id}")
+    # maze.create_logo()
+    # Menu(maze)
+    #   maze.draw_maze(None)
+    #   print(data)
+    #   print(f"L'ID de la cellule (x=0, y=0) est : {print")
+    #   print(f"L'ID de la cellule (x=0, y=10) est : {maze.grid[0][9].cell_id}")
+    #   print(f"L'ID de la cellule (x=2, y=1) est : {maze.grid[1][2].cell_id}")
 
 
 if __name__ == "__main__":
--- utils/parser.py
+++ utils/parser.py
@@ -1,42 +1,49 @@
 import os
-from typing import Literal
+from typing import Callable
+from pydantic import BaseModel, Field, ValidationError
+
+clear: Callable[[], int] = lambda: os.system('cls' if os.name == 'nt'
+                                             else 'clear')
 
 
-def clear() -> None:
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
+class MazeConfig(BaseModel):
+    WIDTH: int = Field(ge=3, le=100)
+    HEIGHT: int = Field(ge=3, le=100)
+    ENTRY_X: int = Field(ge=0, le=100)
+    ENTRY_Y: int = Field(ge=0, le=100)
+    EXIT_X: int = Field(ge=0, le=100)
+    EXIT_Y: int = Field(ge=0, le=100)
+    OUTPUT_FILE: str = Field(default="exit.txt", max_length=20)
+    PERFECT: bool = Field(default=True)
 
 
-def parsing_data(file: str) -> dict | Literal[False]:
-    data: dict = {}
+def parsing_data(file: str) -> MazeConfig | bool:
+    data = {}
     try:
-        with open(file, "r", encoding="utf-8") as content:
-            for line in content:
-                if "WIDTH" in line:
-                    data["WIDTH"] = int(line.split("=")[-1].strip())
-                elif "HEIGHT" in line:
-                    data["HEIGHT"] = int(line.split("=")[-1].strip())
-                elif "ENTRY" in line:
-                    value_entry: str = line.split("=")[-1].strip()
-                    value_entry = value_entry.replace(",", '.')
-                    data["ENTRY"] = float(value_entry)
-                elif "EXIT" in line:
-                    value_exit: str = line.split("=")[-1].strip()
-                    value_exit = value_exit.replace(",", '.')
-                    data["EXIT"] = float(value_exit)
-                elif "OUTPUT_FILE" in line:
-                    value_str: str = line.split("=")[-1].strip()
-                    if value_str == "":
-                        raise ValueError("Error OUTPUT_FILE: add a filename")
-                    data["OUTPUT_FILE"] = str(value_str)
-                elif "PERFECT" in line:
-                    value_bool: str = line.split("=")[-1].strip()
-                    if value_bool not in ("True", "False"):
-                        raise ValueError(f"Error PERFECT : {value_bool}")
-                    data["PERFECT"] = value_bool
-            return data
-    except Exception as error:
-        print(error)
+        with open(file, "r", encoding="utf-8") as f:
+            for line in f:
+                key, value = line.split("=")
+                key = key.strip()
+                value = value.strip()
+                if key == "WIDTH":
+                    data["WIDTH"] = int(value)
+                elif key == "HEIGHT":
+                    data["HEIGHT"] = int(value)
+                elif key == "ENTRY":
+                    x, y = map(int, value.split(","))
+                    data["ENTRY_X"] = x
+                    data["ENTRY_Y"] = y
+                elif key == "EXIT":
+                    x, y = map(int, value.split(","))
+                    data["EXIT_X"] = x
+                    data["EXIT_Y"] = y
+                elif key == "OUTPUT_FILE":
+                    if not value:
+                        raise ValueError("OUTPUT_FILE missing")
+                    data["OUTPUT_FILE"] = value
+                elif key == "PERFECT":
+                    data["PERFECT"] = value.lower() == "true"
+        return MazeConfig(**data)
+    except (Exception, ValidationError) as error:
+        print(f"[ERROR]: {error.errors()[0]['msg']}")
         return False
