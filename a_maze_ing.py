d8d8740ca8ea4580880a007e4926a79fe1ec5d81
Pacome Pilaz <papilaz@z3r11p6.42lyon.fr>
2026-04-13 21:51:34 +0200

    🧩 feat(ajout fonction pour generer n importe quelle nbr en logo de 0 a 99):

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -11,11 +11,11 @@ def main() -> None:
         print(f"[ERROR]: file {file_config}")
         return
     maze = Maze(config)
-    # maze.draw_grid()
     maze.generate_logo()
+    # maze.draw_grid()
     maze.generate_maze()
-    maze.draw_maze()
-    Menu(maze)
+    # maze.draw_maze()
+    # Menu(maze)
 
 
 if __name__ == "__main__":
