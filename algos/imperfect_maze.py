576293342b0d3fd79cf9ee1fea68df9be5c8cba2
Pacome PILAZ <pacomepilaz@gmail.com>
2026-04-22 17:25:19 +0200

    fix bug imperfect maze all is good

--- algos/imperfect_maze.py
+++ algos/imperfect_maze.py
@@ -3,22 +3,27 @@ from maze.utils_enum import Color, Theme
 from utils.parser import clear
 import time
 
+
 def imperfect_maze_func(maze):
     stack = [(maze.entry[0], maze.entry[1])]
     delay: int = Theme.delais_draw
     maze.all_cell_false()
     for height in range(maze.height):
         for width in range(maze.width):
-            nbr_random = randrange(1, 10)
-            cell_destruction = maze.grid[width][height]
-            neighbor = maze.grid[width][height]
-            if height != 0 and width != 0 and height != maze.height and width != maze.width - 1 and cell_destruction.visit is not True:
+            nbr_random = randrange(1, 3)
+            cell_destruction = maze.grid[height][width]
+            if height != 0 and width != 0 and height < maze.height - 1 and width < maze.width and cell_destruction.visit is not True:
+                cell_destruction.visit = True
                 if nbr_random == 1:
-                        cell_destruction.walls["North"] = False
-                        neighbor.walls["South"] = False
-                elif nbr_random == 2:
-                    cell_destruction.walls["West"] = False
-                    neighbor.walls["East"] = False
+                        neighbor = maze.grid[height - 1][width]
+                        if neighbor.visit is not True:
+                            cell_destruction.walls["North"] = False
+                            neighbor.walls["South"] = False
+                if nbr_random == 2:
+                    neighbor = maze.grid[height][width - 1]
+                    if neighbor.visit is not True:
+                        cell_destruction.walls["West"] = False
+                        neighbor.walls["East"] = False
                 # maze.draw_maze()
                 if Theme.animation_draw:
                     maze.draw_maze(False)
